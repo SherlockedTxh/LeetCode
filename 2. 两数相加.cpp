81e94dd01b70d4dc48939294c17942ac166360f9
SherlockedTxh <849366508@qq.com>
2020-03-02 11:25:04 +0800

    Update 2. 两数相加.cpp

--- 2. 两数相加.cpp
+++ 2. 两数相加.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-using namespace std;
-
-  struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode(int x) : val(x), next(NULL) {}
-  };
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
  
 class Solution {
 public:
