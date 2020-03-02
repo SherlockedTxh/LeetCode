04e5372515cc953627969ddcee61a8f8d9c24a51
SherlockedTxh <849366508@qq.com>
2020-03-02 15:03:19 +0800

    Update 3. 无重复字符的最长子串.cpp

--- 3. 无重复字符的最长子串.cpp
+++ 3. 无重复字符的最长子串.cpp
@@ -19,3 +19,25 @@ public:
         return ans;
     }
 };
+
+//方法二 哈希表 时间空间复杂度 O(n)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        //s[start,end) 左包含 右不包含
+        int start(0), end(0), len(0), ans(0);
+        unordered_map<char, int> hash;
+        while(end < s.length()){
+            if(hash.find(s[end]) != hash.end() && hash[s[end]] >= start){
+                //hash.end()是尾后指针，不是指向最后一个元素
+                start = hash[s[end]] + 1;
+                len = end - start;
+            }
+            hash[s[end]] = end;
+            end ++;
+            len ++;
+            ans = max(len, ans);
+        }
+        return ans;
+    }
+};
