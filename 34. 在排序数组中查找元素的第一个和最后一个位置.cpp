6b56b9edd19b7c8200280e339cba2564618732b0
SherlockedTxh <849366508@qq.com>
2020-03-09 15:57:43 +0800

    Update 34. 在排序数组中查找元素的第一个和最后一个位置.cpp

--- 34. 在排序数组中查找元素的第一个和最后一个位置.cpp
+++ 34. 在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -68,3 +68,52 @@ public:
         return ans;
     }
 };
+
+//优化后的方法，两次搜索，分别搜索左右边界，注意while条件、left和right的赋值和更新的变化、以及返回值的处理
+//可以参考这篇文章，关于二分法讲的很详细：
+//https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/solution/er-fen-cha-zhao-suan-fa-xi-jie-xiang-jie-by-labula/
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans;
+        ans.push_back(left_bound(nums, target));
+        ans.push_back(right_bound(nums, target));
+        return ans;
+    }
+    int left_bound(vector<int>& nums, int target){
+        if(nums.size() == 0) return -1;
+        int left(0), right(nums.size()), mid;
+        while(left < right){
+            mid  = left + (right - left) / 2;
+            if(target == nums[mid]){
+                right = mid;
+            }
+            else if(target < nums[mid]){
+                right = mid;
+            }
+            else if(target > nums[mid]){
+                left = mid + 1;
+            }
+        }
+        if(left == nums.size()) return -1;
+        return nums[left] == target ? left : -1;
+    }
+    int right_bound(vector<int>& nums, int target){
+        if(nums.size() == 0) return -1;
+        int left(0), right(nums.size()), mid;
+        while(left < right){
+            mid = left + (right - left) / 2;
+            if(target == nums[mid]){
+                left = mid + 1;
+            }
+            else if(target < nums[mid]){
+                right = mid;
+            }
+            else if(target > nums[mid]){
+                left = mid + 1;
+            }
+        }
+        if(left == 0) return -1;
+        return nums[left - 1] == target ? left - 1 : -1;
+    }
+};
