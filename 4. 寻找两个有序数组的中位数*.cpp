a2346fe0da99cd46b63ca9329734cc8e972bbcb6
SherlockedTxh <849366508@qq.com>
2020-03-12 13:33:23 +0800

    Update 4. 寻找两个有序数组的中位数*.cpp

--- 4. 寻找两个有序数组的中位数*.cpp
+++ 4. 寻找两个有序数组的中位数*.cpp
@@ -1,3 +1,42 @@
+// 大致思路是把两个数组都划分成两部分，两个左边部分放在一起组成left，两个右边部分放在一起组成right，讨论当lefe和right数目相同时中位数的情况
+// 这题需要好好想想才能理解，过程比较复杂，贴一个题解
+// https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/xun-zhao-liang-ge-you-xu-shu-zu-de-zhong-wei-shu-b/
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        if (m > n) { // to ensure m<=n
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+        while(iMin <= iMax){
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+            if(i < iMax && nums2[j - 1] > nums1[i]){
+                iMin = i + 1; // i is too small
+            }
+            else if(i > iMin && nums1[i - 1] > nums2[j]){
+                iMax = i - 1; // i is too big
+            }
+            else{ // i is perfect
+                int maxLeft = 0;
+                if(i == 0) maxLeft = nums2[j - 1];
+                else if(j == 0) maxLeft = nums1[i - 1];
+                else maxLeft = max(nums1[i - 1], nums2[j - 1]);
+                if((m + n) % 2 == 1) return maxLeft;
+
+                int minRight = 0;
+                if(i == m) minRight = nums2[j];
+                else if(j == n) minRight = nums1[i];
+                else minRight = min(nums1[i], nums2[j]);
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
+};
+
+//原方法 复杂度O((m+n)/2)
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
