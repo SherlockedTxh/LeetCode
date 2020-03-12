7f4fd48b8a42c7fa0e577568c5a30f460f6a20f7
SherlockedTxh <849366508@qq.com>
2020-03-12 20:15:32 +0800

    Update 78. 子集.cpp

--- 78. 子集.cpp
+++ 78. 子集.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 // 思路：先往ans中加入一个空数组，对于每个nums中的数字进行如下操作：
 // 对于ans中的每个数组，往ans中加入该数字与这个数组组成的新数组，(不修改原数组)
+// 另一种思路：也可以用二进制位来表示元素的选取
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
