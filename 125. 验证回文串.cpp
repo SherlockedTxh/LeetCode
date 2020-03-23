fbecc01f095fd580c68753859405163da458d863
SherlockedTxh <849366508@qq.com>
2020-03-23 19:48:51 +0800

    Add files via upload

---
+++ 125. 验证回文串.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <limits>
+using namespace std;
+
+// 双指针 一趟写下来居然一个bug也没有直接击败98.62%/100%通过，可把我牛逼坏了，插会儿腰
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int len = s.size();
+        int left = 0, right = len - 1;
+        while(left < right){
+            if(check(s[left]) && check(s[right])){
+                if(s[left] != s[right]){
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            else if(!check(s[left]) && check(s[right])){
+                left++;
+            }
+            else if(check(s[left]) && !check(s[right])){
+                right--;
+            }
+            else{
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+
+    bool check(char& a){
+        if((a >= 'a' && a <= 'z') || (a >= '0' && a <= '9')){
+            return true;
+        }
+        else if((a >= 'A' && a <= 'Z')){
+            a += 32;
+            return true;
+        }
+        return false;
+    }
+};
