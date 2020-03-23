d37983b31b598d47eac81b212971d9cd97ec0886
SherlockedTxh <849366508@qq.com>
2020-03-23 11:48:23 +0800

    Update 98. 验证二叉搜索树.cpp

--- 98. 验证二叉搜索树.cpp
+++ 98. 验证二叉搜索树.cpp
@@ -15,6 +15,28 @@ struct TreeNode {
 };
 
 // 方法一：递归，记录当前节点取值上下界
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return check(root, -INFINITY, INFINITY);
+    }
+
+    bool check(TreeNode* root, double low, double high){
+        if(root == NULL) return true;
+
+        if(root->val <= low) return false;
+        if(root->val >= high) return false;
+
+        if(!check(root->left, low, root->val)){
+            return false;
+        }
+        if(!check(root->right, root->val, high)){
+            return false;
+        }
+        return true;
+    }
+};
+
 // 方法二：中序遍历，遍历的数字应该是从小到大
 // Notes: 这题有一个测试用例是[-2147483648]，正好是INT_MIN
 class Solution {
