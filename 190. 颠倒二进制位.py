f549e0ef5c339fd9baa709832f83bce757088688
SherlockedTxh <849366508@qq.com>
2020-03-25 18:29:43 +0800

    Add files via upload

---
+++ 190. 颠倒二进制位.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+from collections import OrderedDict
+
+# n右移31-i得到的数字的最后一位是结果数字的第i位
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ans = 0
+        for i in range(0, 32):
+            ans += ((n >> (31 - i)) & 1) << i
+        return ans
