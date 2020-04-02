99b025d44314490302a7231e923ca8fa9e780d30
SherlockedTxh <849366508@qq.com>
2020-04-02 18:13:18 +0800

    Update 28. 实现 strStr().cpp

--- 28. 实现 strStr().cpp
+++ 28. 实现 strStr().cpp
@@ -7,6 +7,7 @@
 #include <limits>
 using namespace std;
 
+// 方法一：逐个匹配
 class Solution {
 public:
     int strStr(string haystack, string needle) {
@@ -30,6 +31,37 @@ public:
                 if(flag) return pos;
             }
             pos++;
+        }
+        return -1;
+    }
+};
+
+// 方法二：KMP
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int len1 = haystack.size(), len2 = needle.size();
+        int j = 0, k = -1;
+        if(len2 == 0) return 0;
+        if(len1 == 0) return -1;
+        vector<int> next = vector<int>(len2 + 1);
+        next[0] = -1;
+        while(j < len2){
+            while(k >= 0 && needle[j] != needle[k]) k = next[k];
+            j++;
+            k++;
+            next[j] = k;
+        }
+
+        int i = 0;
+        j = 0;
+        while(i < len1){
+            while(j >= 0 && needle[j] != haystack[i]) j = next[j];
+            i++;
+            j++;
+            if(j == len2){
+                return i - j;
+            }
         }
         return -1;
     }
