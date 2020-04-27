1c1618a68a088a03e65c83225ea7f56f9c81434e
SherlockedTxh <849366508@qq.com>
2020-04-27 10:00:43 +0800

    Add files via upload

---
+++ 372. 超级次方.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int superPow(int a, vector<int>& b) {
+        if(b.size() == 0) return 1;
+        int last = b.back();
+        b.pop_back();
+
+        int part1 = mypow(a, last, 1337);
+        int part2 = mypow(superPow(a, b), 10, 1337);
+        return (part1 * part2) % 1337;
+    }
+
+    int mypow(int a, int b, int base){
+        int ans = 1;
+        a %= base;
+        while(b){
+            if(b & 1) ans = (ans * a) % base;
+            a = (a * a) % base;
+            b >>= 1;
+        }
+        return ans;
+    }
+};
