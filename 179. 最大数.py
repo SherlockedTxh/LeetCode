d4c3696bccedc24b94dd14e47054de36a6119443
SherlockedTxh <849366508@qq.com>
2020-04-28 09:36:45 +0800

    Update 179. 最大数.py

--- 179. 最大数.py
+++ 179. 最大数.py
@@ -18,8 +18,6 @@ class Solution:
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         from functools import cmp_to_key
-        if not nums:
-            return ''
         nums = map(str, nums)
         key = cmp_to_key(lambda x, y: int(y + x) - int(x + y))
         # lstrip() 方法: 截掉字符串左边的空格或指定字符  0012->12
