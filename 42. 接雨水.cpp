e084de785d3fe8510dd4739f0bda3f2848f8cf85
SherlockedTxh <849366508@qq.com>
2020-07-30 16:23:09 +0800

    Update 42. 接雨水.cpp

--- 42. 接雨水.cpp
+++ 42. 接雨水.cpp
@@ -22,3 +22,23 @@ public:
         return ans;
     }
 };
+
+// 双指针
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int max_left = 0, max_right = 0, ans = 0;
+        while(left < right){
+            if(height[left] < height[right]){
+                height[left] >= max_left ? (max_left = height[left]) : ans += (max_left - height[left]);
+                left++;
+            }
+            else{
+                height[right] >= max_right ? (max_right = height[right]) : ans += (max_right - height[right]);
+                right--;
+            }
+        }
+        return ans;
+    }
+};
