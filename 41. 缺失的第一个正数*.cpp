b6dc8f3b519fa1f0d45d237837fdf530446e096e
SherlockedTxh <849366508@qq.com>
2020-07-30 15:51:36 +0800

    Update 41. 缺失的第一个正数*.cpp

--- 41. 缺失的第一个正数*.cpp
+++ 41. 缺失的第一个正数*.cpp
@@ -50,3 +50,40 @@ public:
         return len + 1;
     }
 };
+
+// 简化版
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for(int& num : nums){
+            if(num <= 0) num = n + 1;
+        }
+        for(int i = 0; i < n; i++){
+            int num = abs(nums[i]);
+            if(num <= n) nums[num - 1] = -abs(nums[num - 1]);
+        }
+        for(int i = 0; i < n; i++){
+            if(nums[i] > 0) return i + 1;
+        }
+        return n + 1;
+    }
+};
+
+// 方法二 置换
+// 通过置换把每个数字归为到对应的位置，如[3,4,1,-1]->[1,-1,3,4]不是正数的位置就是答案
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            while(nums[i] > 0 and nums[i] <= n and nums[nums[i] - 1] != nums[i]){
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+        for(int i = 0; i < n; i++){
+            if(nums[i] != i + 1) return i + 1;
+        }
+        return n + 1;
+    }
+};
