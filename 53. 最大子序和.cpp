5e087a16c569ef75ea57ecc48b065fec7e9bb9aa
SherlockedTxh <849366508@qq.com>
2020-07-31 14:47:36 +0800

    Update 53. 最大子序和.cpp

--- 53. 最大子序和.cpp
+++ 53. 最大子序和.cpp
@@ -9,13 +9,37 @@ using namespace std;
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int len = nums.size(), ans(nums[0]);
-        vector<int> dp(len);
-        dp[0] = nums[0];
-        for(int i = 1; i < len; i ++){
-            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
-            ans = max(ans, dp[i]);
+        int ans = nums[0], sum = 0, len = nums.size();
+        for(int i = 0; i < len; i++){
+            sum = max(sum + nums[i], nums[i]);
+            ans = max(ans, sum);
         }
         return ans;
+    }
+};
+
+// 分治（线段树）
+class Solution {
+public:
+    struct Status{
+        int lsum, rsum, msum, isum;
+    };
+    Status pushup(Status l, Status r){
+        int isum = l.isum + r.isum;
+        int lsum = max(l.lsum, l.isum + r.lsum);
+        int rsum = max(r.rsum, r.isum + l.rsum);
+        int msum = max(max(l.msum, r.msum), l.rsum + r.lsum);
+        Status a = {lsum, rsum, msum, isum};
+        return a;
+    }
+    Status get(vector<int> &a, int l, int r){
+        if(l == r) return Status{a[l], a[l], a[l], a[l]};
+        int mid = (l + r) >> 1;
+        Status left = get(a, l, mid);
+        Status right = get(a, mid + 1, r);
+        return pushup(left, right);
+    }
+    int maxSubArray(vector<int>& nums) {
+        return get(nums, 0, nums.size() - 1).msum;
     }
 };
