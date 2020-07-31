1a471b408b3115b13484ece0fe688eb4d930dd78
SherlockedTxh <849366508@qq.com>
2020-07-31 15:59:01 +0800

    Update 66. 加一.cpp

--- 66. 加一.cpp
+++ 66. 加一.cpp
@@ -12,7 +12,7 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int len = digits.size();
-        for(int i = len - 1; i >= 0; i++){
+        for(int i = len - 1; i >= 0; i--){
             if(digits[i] != 9){
                 digits[i]++;
                 return digits;
