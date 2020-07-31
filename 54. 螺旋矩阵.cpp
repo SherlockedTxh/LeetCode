de6fd374dcdc7da3581cf5be1cf55b12c6d5faf6
SherlockedTxh <849366508@qq.com>
2020-07-31 15:17:11 +0800

    Update 54. 螺旋矩阵.cpp

--- 54. 螺旋矩阵.cpp
+++ 54. 螺旋矩阵.cpp
@@ -41,3 +41,37 @@ public:
         return ans;
     }
 };
+
+// 方法二：按层模拟
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        if (matrix.size() == 0 || matrix[0].size() == 0) {
+            return {};
+        }
+        int rows = matrix.size(), columns = matrix[0].size();
+        vector<int> ans;
+        int left = 0, right = columns - 1, top = 0, bottom = rows -1;
+        while(left <= right and top <= bottom){
+            for(int column = left; column <= right; column++){
+                ans.push_back(matrix[top][column]);
+            }
+            for(int row = top + 1; row <= bottom; row++){
+                ans.push_back(matrix[row][right]);
+            }
+            if(left < right and top < bottom){
+                for(int column = right - 1; column > left; column--){
+                    ans.push_back(matrix[bottom][column]);
+                }
+                for(int row = bottom; row > top; row--){
+                    ans.push_back(matrix[row][left]);
+                }
+            }
+            left++;
+            right--;
+            top++;
+            bottom--;
+        }
+        return ans;
+    }
+};
