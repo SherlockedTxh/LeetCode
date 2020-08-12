0b8354a7da712bfe728e657b5088a246811b2f4e
SherlockedTxh <849366508@qq.com>
2020-08-12 16:28:55 +0800

    Update 125. 验证回文串.cpp

--- 125. 验证回文串.cpp
+++ 125. 验证回文串.cpp
@@ -46,3 +46,23 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int left = 0, right = n - 1;
+        while(left < right){
+            while(left < right and !isalnum(s[left])) left++;
+            while(left < right and !isalnum(s[right])) right--;
+            if(left < right){
+                if(tolower(s[left]) == tolower(s[right])){
+                    left++;
+                    right--;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+};
