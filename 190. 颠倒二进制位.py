a373517eb07ab129e53bd32cd3b7eb8aa8678855
SherlockedTxh <849366508@qq.com>
2020-08-27 16:27:49 +0800

    Update 190. 颠倒二进制位.py

--- 190. 颠倒二进制位.py
+++ 190. 颠倒二进制位.py
@@ -7,3 +7,27 @@ class Solution:
         for i in range(0, 32):
             ans += ((n >> (31 - i)) & 1) << i
         return ans
+
+// 这个算法为用 3 个操作反转一个字节中的位
+class Solution {
+public:
+    uint32_t reverseByte(uint32_t byte, map<uint32_t, uint32_t> cache) {
+        if (cache.find(byte) != cache.end()) {
+            return cache[byte];
+        }
+        uint32_t value = (byte * 0x0202020202 & 0x010884422010) % 1023;
+        cache.emplace(byte, value);
+        return value;
+    }
+
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ret = 0, power = 24;
+        map<uint32_t, uint32_t> cache;
+        while (n != 0) {
+            ret += reverseByte(n & 0xff, cache) << power;
+            n = n >> 8;
+            power -= 8;
+        }
+        return ret;
+    }
+};
